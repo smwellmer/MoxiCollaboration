7a33c42837ce55c07bf5ee53cf6dec6e41097297
Samantha Wellmer <sam@Samanthas-MacBook-Air.local>
2022-05-23 20:37:14 -0400

    adding links

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Header from "./Components/Header";
 import Footer from "./Components/Footer";
 import Main from "./Components/Main";
 import Button from "./Components/Button";
-
+import {Route, Routes, Link} from 'react-router-dom';
 
 function App() {
 
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,8 +1,14 @@
+import {Link} from 'react-router-dom';
+import About from './About';
+
+
 function Header() {
     return <header>
-       <img src="/MoxiLogo.png"></img>
-        
-        
+       <img src="/MoxiLogo.png" path="/"></img>
+       <Link to="about" element={<About/>}>About</Link>
+       <Link to="contact">Contact</Link>
+       <Link to="donate">Donate</Link>
+     
         </header>
 }
 
