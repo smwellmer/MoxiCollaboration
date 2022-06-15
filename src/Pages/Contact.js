8222bf52c419a037a74a9e962966622acb520f33
Samantha Wellmer <sam@Samanthas-MacBook-Air.local>
2022-06-15 18:54:28 -0400

    contact page

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -1,5 +1,14 @@
+import { FaTwitterSquare, FaFacebookSquare, FaInstagram} from "react-icons/fa";
+
 function Contact() {
-    return <div>Contact</div>
+    return <div>
+        <p> You can find us on Facebook, Instagram, and Twitter!</p>
+        <a href="https://www.facebook.com/moxicollaboration"><FaFacebookSquare/></a>
+        <a href="https://www.instagram.com/moxicollaboration/"><FaInstagram/></a>
+        <a href="https://twitter.com/moxicollab"><FaTwitterSquare/></a>
+        <p>To contact us directly, email us at moxicollaboration@gmail.com</p>
+
+    </div>
 }
 
 export default Contact
