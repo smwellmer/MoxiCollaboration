95aa885d32d8c2986cfbc36b217700b086b68a9c
Samantha Wellmer <sam@Samanthas-MacBook-Air.local>
2022-06-21 20:46:10 -0400

    finish home page

--- src/Pages/Main.js
+++ src/Pages/Main.js
@@ -4,23 +4,17 @@ import Button from "../Components/Button"
 function Main() {
     return <div className='main'>
       
-        <h1>Moxi</h1>
-        <h1>Collaboration</h1>
+        <h1 className="title">Moxi Collaboration</h1>
         
-        <p className="mission">
-        <h2>Our mission:</h2>
-        <h3>"To provide comfort, dignity, and empowerment for survivors of sexual assault."</h3>
-        </p>
+        <div className="mission">
+        Our mission: "To provide comfort, dignity, and empowerment for survivors of sexual assault."
+        </div>
 
-        <p>
-        <h2>mox·ie</h2>
-        <h2>/ˈmäksē/ </h2>
-        <h2>force of character, determination, or nerve.</h2>
-        </p>
+        <img src="MoxieDefinition-removebg-preview.png"className="moxieDefinition"/>
 
        
        
-        <p>
+        <p className="statement">
          “From my own personal experience, the most valued thing you need in that moment is comfort. 
          The ability to not only cleanse your body with products that give you a nostalgic sense of safety, 
          but to put on clothes that give you added comfort and support and security - it’s unmatched and an 
