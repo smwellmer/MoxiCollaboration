51fe4aa8e5f77e0d1ad6fe5ec627fc2b75afa2b3
Samantha Wellmer <sam@Samanthas-MacBook-Air.local>
2023-01-23 20:18:40 -0500

    edit donate page

--- src/Pages/Donate.js
+++ src/Pages/Donate.js
@@ -7,8 +7,9 @@ function Donate() {
     return <div className="donate">
         <p>Donating to Moxi Collaboration will allow our organization to assemble comfort kits for survivors of sexual assault.</p>
 
-        <p>While our 501(c)(3) status is pending, we are accepting donations via GoFundMe. 
-        In the future, we are planning to move on to other platforms that will better support our organization's needs.</p>
+        <p>As of July 12, 2022 Moxi Collaboration is an official 501(c)(3) non-profit organization. </p>
+
+        <p>If you wish to donate, the link below will take you to our current GoFundMe page.</p>
 
         <a href="https://www.gofundme.com/f/bggbg4?utm_campaign=p_cf+share-flow-1&utm_medium=email&utm_source=customer" target="_blank" rel="noreferrer noopener"><button className="donateButton">Donate Now</button></a>
 
